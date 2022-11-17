00c0b014468c24b19e6b45d52b1cfd218cb1f683
Francisco Patricio <fpatricio1@proton.me>
2022-11-17 14:24:33 +0000

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Movie.destroy_all
+puts 'Creating 4 fake movies...'
+
+  4.times do
+    Movie.create(
+      title:Faker::Book.title,
+      overview:Faker::Restaurant.review,
+      poster_url:Faker::LoremPixel.image(size: "50x60"),
+      rating:Faker::Number.number
+    )
+  end
+  puts 'Done!'
